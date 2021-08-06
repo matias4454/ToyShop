760be2fd57bbfbbc4acc7528ab48da466c6b6d27
EC2 Default User <ec2-user@ip-172-31-28-213.us-east-2.compute.internal>
2021-08-06 12:32:07 +0000

    DetailsViewComponent registered in app module

---
+++ src/app/components/accessories/details-view/details-view.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from '../../../services/data.service';
+import { Product } from '../../../models/Product';
+import { MessageService } from '../../../services/message.service';
+
+@Component({
+  selector: 'app-acc-details',
+  templateUrl: './details-view.component.html',
+  styleUrls: ['./details-view.component.css']
+})
+export class DetailsViewComponent implements OnInit {
+
+    private subscription: Subscription;
+    private selectedIndex: number;  
+
+
+  constructor(private dataSvc: DataService, private msgService: MessageService) 
+  {
+    this.selectedIndex = -1;
+      this.subscription = msgService.onMessage().subscribe(msg =>
+      {
+        this.selectedIndex = msg;
+         
+      });
+  }
+
+  ngOnInit(): void {
+
+  }
+  
+
+  
+    public getList(): Product[]    
+    {
+        return this.dataSvc.getToys('');
+    }
+    
+    public getSelectedIndex() : number
+    {
+        return this.selectedIndex;
+    }
+    
+    public setSelectedIndex(ix : number) 
+    {
+        this.selectedIndex = ix;
+    }
+    
+    public getSelectedProducts() : Product[]
+    {        
+        return this.getList().filter(p => p.getId() == this.selectedIndex);
+    }
+
+}
